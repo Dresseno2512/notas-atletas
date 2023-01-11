4ce19cab6a0efeb4af76980f6b38c3e7e1929347
Dresseno2512 <eduardodresseno@gmail.com>
2023-01-10 21:10:18 -0300

    Create projeto1.js

---
+++ projeto1.js
@@ -0,0 +1,38 @@
+        let atletas = [{
+            nome: "Cesar Abascal",
+            notas: [10, 9.34, 8.42, 10, 7.88]
+        }, {
+            nome: "Fernando Puntel",
+            notas: [8, 10, 10, 7, 9.33]
+        }, {
+            nome: "Daiane Jelinsky",
+            notas: [7, 10, 9.5, 9.5, 8]
+        }, {
+            nome: "Bruno Castro",
+            notas: [10, 10, 10, 9, 9.5]
+        }]
+
+        function retornaListaNotas(atletas) {
+            let resultados = [];
+            for (const element of atletas) {
+                let notasValidas = element.notas.sort().slice(1, element.notas.length - 1);
+                let somaDasNotas = 0;
+                let somaDasNotasValidas = notasValidas.reduce(
+                    (acumulador, valorAtual) => acumulador + valorAtual,
+                    somaDasNotas
+                );
+                let mediaValida = somaDasNotasValidas / notasValidas.length;
+                console.log('Atleta: ' + element.nome);
+                console.log('Notas Obtidas:' + element.notas.sort().join(","));
+                console.log('Média Válida: ' + mediaValida);
+                console.log('');
+                //adiciona a média calculada à matriz resultados
+                resultados.push({
+                    nome: element.nome,
+                    notas: element.notas.sort(),
+                    media: mediaValida,
+                });
+            }
+
+            return (resultados);
+        }
